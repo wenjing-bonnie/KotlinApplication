40da07a649bbf4acb7492be08f91a8a856c0994e
wenjing.liu <liuwenjing.12@163.com>
2022-03-11 09:40:25 +0800

    method type

--- app/src/main/java/com/wj/kotlin/KotlinContent1.kt
+++ app/src/main/java/com/wj/kotlin/KotlinContent1.kt
@@ -12,22 +12,24 @@ package com.wj.kotlin
  *   val method = { a:Int,b:Int -> dd
  *         a+b
  *   }
- *   (3)只有一个输入参数的时候,在{}默认的就会有一个it的输入参数
+ *   (3)只有一个输入参数的时候,在faf{}默认的就会有一个it的输入参数
  * 2.函数类型的变量可[解决java通过接口回调的方式来进行处理结果]
- *   (1)通常方法的输入参数中有一个[函数类型的变量]来代替[java的接口类型的输入参数]
+ *   (1)通常方法的[输入参数中有一个函数类型的变量]来代替[java的接口类型的输入参数]
  *   inline fun login(user: String, password: String, response: (String, Int) -> String) {
  *
  *   }
- *   (2)使用函数类型的输入参数的时候,该方法需要用[inline 内联]来进行修饰:
+ *   (2)使用函数类型的输入参数的时候,该方法需要用[inline内联]来进行修饰:
  *   可以将回调函数直接拷贝到调用处,避免转换成字节码的时候,需要创建对象,引起性能损耗
  *   (3)在调用该内联函数,对函数类型的输入变量进行赋值的时候,有两种方式:
- *      1)lambda表达式：lambda表达式本质就是一个函数类型
+ *      1)lambda表达式：lambda表达式本质就是一个函数类型(这种方式可直接跟在调用函数的后面)
  *       { user:String, password:String ->
  *        //具体实现的内容
- *        }
+ *       } //(这种又称为匿名函数)
  *      2）在回调函数名前面添加::
- *      ::handleResponse
- * 3.
+ *      ::handleResponse //(这种又称为具名函数)
+ * 3.函数类型可作为返回值 [应用场景是干什么呢] TODO ？？？？？？
+ *   那么就可以将(2.3)中的两种方式作为返回值，即是函数类型的返回值
+ *
  * 4.
  *
  */
@@ -44,7 +46,8 @@ private fun niMing() {
 }
 
 /**
- * 2.函数类型&隐式返回
+ * 1.函数类型&隐式返回
+ * （1.1实例）
  *  函数可作为传入参数，分为两部分
  *  (1)可以定义一个变量为函数类型,一个函数类型格式:(输入参数类型)-> 返回值类型
  *  (2)直接调用变量进行传入值即可
@@ -63,6 +66,7 @@ private fun methodAction() {
     //3.调用此函数
     println(method(30))
     /**
+     * （1.2实例）
      *  3.函数类型自动类型推断,即上面的 (1)可以直接省略,自动根据最后一行推断函数的返回值
      *  4.匿名函数等价于lambda表达式
      */
@@ -74,6 +78,7 @@ private fun methodAction() {
 }
 
 /**
+ * （2.1-2.2实例）
  * 5.定义一个函数类型的功能:用来实现一个原java的接口回调
  * 若一个函数中有lambda表达式，最好用inline进行修饰
  * 若不使用内联,会生成多个对象调用，造成性能损耗
@@ -90,7 +95,9 @@ private inline fun login(user: String, password: String, response: (String, Int)
 
 fun main() {
     methodAction()
+
     /**
+     * （2.3实例）
      * 6.在对lambda表达式进行赋值的时候
      * (1)一种是通过lambda表达式来进行赋值
      */
@@ -101,8 +108,29 @@ fun main() {
      * (2)通过函数类型的对象:在函数名前面添加::
      */
     login("liuwenjing", "123456", ::handleResponse)
+
+    /**
+     * （3实例）的调用
+     */
+    val method = returnMethod(3, 4)
+    println(method("3", 6))
 }
 
 private fun handleResponse(msg: String, code: Int): String {
+    println("msg:$msg , code:${code}")
     return "msg:$msg , code:${code}"
+}
+
+/**
+ * （3实例）
+ * 返回一个函数类型的返回值
+ */
+private fun returnMethod(a: Int, b: Int): (String, Int) -> String {
+    val firstSum = a + b
+//    return { aa: String, bb: Int ->
+//        println("$a+$b=?")
+//        val sum = aa + bb + firstSum
+//        sum
+//    }
+    return ::handleResponse
 }
