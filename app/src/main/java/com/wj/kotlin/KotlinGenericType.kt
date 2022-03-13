ac6dbfaf34aa7d9b9371ff11f808fa774d797bb9
wenjing.liu <liuwenjing.12@163.com>
2022-03-13 15:04:17 +0800

    generic type

--- app/src/main/java/com/wj/kotlin/KotlinGenericType.kt
+++ app/src/main/java/com/wj/kotlin/KotlinGenericType.kt
@@ -2,6 +2,36 @@ package com.wj.kotlin
 
 /**
  * create by wenjing.liu at 2022/3/13
+ * 泛型
  */
-class KotlinGenericType {
+class Generic<T>(val obj: T, val isObj: Boolean) {
+    fun show(obj: T) {
+        println("outputs: $obj")
+    }
+
+    fun getGenericObj() = obj.takeIf {
+        isObj
+    }
+}
+
+fun <B> show(item: B) {
+    item?.also {
+
+    }
+}
+
+/**
+ * 模仿Rxjava
+ */
+class RxJava<T>(val isMap: Boolean, var input: T) {
+    /**
+     * T是要变换的类型,R是变换后的类型
+     */
+    fun <R> map(mapAction: (T) -> R) {
+
+    }
+}
+
+fun main() {
+    println(Generic("123", true).getGenericObj())
 }
