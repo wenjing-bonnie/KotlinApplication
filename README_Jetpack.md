83d213041e26e19359004c04d8de911087f1e61e
wenjing.liu <liuwenjing.12@163.com>
2022-03-20 14:55:11 +0800

    ad

--- README_Jetpack.md
+++ README_Jetpack.md
@@ -3,22 +3,43 @@
 MVVM+Jetpack Google标准化Jetpack架构模式 未来的趋势
 
 ## 一、Compose 声明式UI
+
 ### 1。
+
 * 声明式UI，改变的式UI的状态。
 * Compose UI组合方式。代替之前View ViewGroup 继承关系
 * 自动更新策略
 * 只会进行一次测量，替换之前的多次测量
 
+### 2.UI相关
 
-
-
-
-
-
-
-
-
-
+* @Compose：用来标记构建View的方法
+* @Preview：在不运行APP的情况下确认布局。常见参数有：
+    - name:String：该名字会显示在预览布局中。
+    - showBackground:Boolean：是否显示背景。
+    - backgroundColor:Long：设置背景颜色。
+    - showDecoration:Boolean：是否显示statusbar和toolbar。
+    - group:String：设置group名字，在UI中分组显示
+    - fontScale:Float：对预览字体放大，范围从0.01
+    - widthDp:Int：最大宽度，单位dp
+    - heightDp:Int：最大高度，单位dp
+* Modifier：用于设置UI摆放位置、padding等信息。链式调用
+    - Modifier.padding()：设置的值必须为Dp。在Int中扩展一个方法dp，最终会转换成Dp。共四个重载方法。例如20.dp
+    - Modifier.plus(otherModifier)：可以把其他的modifier加入到当前Modifier中
+    - Modifier.fillMaxHeight/fillMaxWidth/fillMaxSize。类似于match_parent
+    - Modifier.width/height/size：设置content的宽和高
+    - Modifier.widthIn/heightIn/sizeIn：设置content的宽和高的最大值和最小值。
+    - Modifier.Modifier.gravity(Alignment.CenterHorizontally)：设置在Column中的元素位置 [TODO 不知道为什么没有提示处理]
+    - Modifier.rtl/ltr：开始布局的方法。从右向左/从左向右。[TODO 不知道为什么没有提示处理]
+* Column/Row ：类似于纵向/横向的ViewGroup
+    - modifier: Modifier
+    - horizontalArrangement: Arrangement.Horizontal
+    - verticalAlignment: Alignment.Vertical
+    - 通常Column需要传入 Alignment.Vertical，row传入
+      Arrangement.Horizontal。也可传入Center、Start、End、SpaceEvenly、SpaceBetween、SpaceAround。
+        - SpaceEvenly：各个元素的空隙等比例，每个元素的左右都有间隙。
+        - SpaceBetween：第一个之前和最后一个之后没有空隙，其他按等比例空隙放入各个元素之间。
+        - SpaceAround：第一个之前和最后一个之后的间隙是一半，其他按2倍的间隙放入到其他各元素之间。
 
 ## 一、
 
--- app/src/main/java/com/wj/kotlin/ui/FirstComposeActivity.kt
+++ app/src/main/java/com/wj/kotlin/ui/FirstComposeActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.MeasurePolicy
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.wj.kotlin.R
 import com.wj.kotlin.ui.ui.theme.KotlinApplicationTheme
 
@@ -20,8 +30,7 @@ class FirstComposeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //对ComponentActivity的扩展函数
         setContent {
-            Greeting("zhang san")
-            GreetingImage()
+            Item()
         }
     }
 }
@@ -32,16 +41,110 @@ fun Greeting(name: String) {
 }
 
 @Composable
-
 fun GreetingImage() {
-    Image(painter = painterResource(id = R.mipmap.ic_launcher), contentDescription = null)
+    Image(painter = painterResource(id = R.drawable.ic_launcher123), contentDescription = null)
 }
 
-@Preview(showBackground = false)
+@Composable
+fun Item() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher123),
+            contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier
+                .width(12.dp)
+                .height(12.dp)
+                .padding(10.dp, 20.dp)
+        )
+        Column {
+            Text(
+                text = "title", modifier = Modifier
+                    .background(color = Color.Red)
+                    .fillMaxWidth()
+                    .sizeIn()
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(10.dp)
+                    .background(color = Color.Red)
+            )
+            Text(text = "message")
+        }
+    }
+}
+
+@Composable
+fun Column() {
+    Row(
+        modifier = Modifier
+            .background(Color.Cyan)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
+
+
+
+    ) {
+        Text(
+            text = "123", modifier = Modifier
+                .background(Color.Red, CircleShape)
+                .padding(10.dp)
+            // .clip(CircleShape)
+            //  .border(1.dp, Color.Black, CircleShape)
+        )
+        Text(
+            text = "456",
+            modifier = Modifier
+                .background(Color.Green, RectangleShape)
+                .padding(20.dp)
+        )
+        Text(text = "789", modifier = Modifier.background(Color.Yellow))
+        Text(text = "012", modifier = Modifier.background(Color.Blue))
+    }
+}
+
+@Composable
+fun HelloList() {
+    LazyColumn(content = {
+        item { Item() }
+        item { Item() }
+        item { Item() }
+    })
+}
+
+//自定义Row
+// ViewGroup onmeasure、onlayout、onDraw ->
+//
+@Composable
+fun ParentLayout(modifier: Modifier, content: @Composable () -> Unit) {
+    //1。对布局进行测量
+    val measurePolicy = MeasurePolicy { measurables, constraints ->
+        //盒子模型
+        val placeable = measurables.map { child ->
+            //设置child的宽高
+            child.measure(constraints = constraints)
+
+        }
+        //放置child
+        layout(constraints.minWidth, constraints.maxHeight) {
+            //placeable.
+        }
+
+    }
+}
+
+@Preview
 @Composable
 fun DefaultPreview() {
-    //KotlinApplicationTheme {
-        Greeting("Android")
-        GreetingImage()
-   /// }
+    Column {
+        HelloList()
+        Spacer(
+            modifier = Modifier
+                .background(Color.Gray)
+                .padding(10.dp)
+        )
+        Column()
+    }
+
 }
