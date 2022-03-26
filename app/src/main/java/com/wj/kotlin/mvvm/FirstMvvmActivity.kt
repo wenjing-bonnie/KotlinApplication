7ed663d72aa9f88f8e583305fdcfae8ba1d55d52
wenjing.liu <liuwenjing.12@163.com>
2022-03-26 19:05:39 +0800

    password

--- app/src/main/java/com/wj/kotlin/mvvm/FirstMvvmActivity.kt
+++ app/src/main/java/com/wj/kotlin/mvvm/FirstMvvmActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import androidx.lifecycle.*
 import com.wj.kotlin.R
 import com.wj.kotlin.databinding.ActivityFirstMvvmBinding
+import com.wj.kotlin.databinding.ActivityFirstMvvmMessageBinding
 import kotlinx.coroutines.launch
 
 class FirstMvvmActivity : AppCompatActivity() {
@@ -15,17 +16,20 @@ class FirstMvvmActivity : AppCompatActivity() {
 
     // viewModels()
     private lateinit var binding: ActivityFirstMvvmBinding
+    private lateinit var messageBinding: ActivityFirstMvvmMessageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //1.初始化ViewModel，用来为页面UI渲染获取数据
         viewModel = ViewModelProvider(this)[FirstMvvmViewModel::class.java]
         //2.初始化ViewBinding，用来实例化控件，可以直接通过"binding."的方式调用控件
         binding = ActivityFirstMvvmBinding.inflate(layoutInflater)
+        messageBinding = ActivityFirstMvvmMessageBinding.bind(binding.root)
         //需要将binding.root传入到setContentView中，让Activity绑定布局。
         //一定不要在传入布局id，否则会添加多次，同时监听器也会添加到错误的布局对象中。
         setContentView(binding.root)
         //在使用控件的时候，可以直接使用Binding对象中的属性，无需findViewById
-        binding.tvText.setText("初始赋值")
+        binding.tvTitle.setText("初始赋值")
         //3.设置生命周期的监听，可以将与生命周期相关的内容转移到Observer中进行处理
         lifecycle.addObserver(FirstLifecycleObserver())
         //4.为viewModel的LiveData设置Observer，可以在被观察对象发生变化的时候，回调到onChanged，从而渲染UI
@@ -36,13 +40,11 @@ class FirstMvvmActivity : AppCompatActivity() {
 //        }
         val observer = Observer<String> { name ->
             println("变化了的name为：$name")
-            binding.tvText.setText(name)
+            binding.tvTitle.setText(name)
         }
         viewModel.currentName.observe(this, observer)
 
-        binding.btnButton.setOnClickListener {
-            btnClickListener()
-        }
+
 
 
 
